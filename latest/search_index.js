8623e4162bbafc5cda7fa83db3af52b26b7e6b04
autodocs <autodocs>
2017-09-28 14:37:43 +0000

    build based on 272f13c

--- latest/search_index.js
+++ latest/search_index.js
@@ -10,9 +10,17 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "Convolution/convolution.html#",
-    "page": "-",
-    "title": "-",
+    "page": "Convolution",
+    "title": "Convolution",
     "category": "page",
+    "text": ""
+},
+
+{
+    "location": "Convolution/convolution.html#Convolution-1",
+    "page": "Convolution",
+    "title": "Convolution",
+    "category": "section",
     "text": ""
 },
 
@@ -29,23 +37,39 @@ var documenterSearchIndex = {"docs": [
     "page": "PDE 1",
     "title": "PDE 1",
     "category": "section",
-    "text": "Show case ported from:Kuramoto-Sivashinksy-benchmark<dl>   <dt>Definition list</dt>   <dd>Is something people use sometimes.</dd><dt>Markdown in HTML</dt>   <dd>Does not work very well. Use HTML <em>tags</em>.</dd> </dl><dl> <video width=\"320\" height=\"240\" controls> <source src=\"assets/pd1.webm\" type=\"video/webm\"> Your browser does not support the video tag. </video> </dl>"
+    "text": "Show case ported from:Kuramoto-Sivashinksy-benchmark  <dt>Definition list</dt>\n  <dd>Is something people use sometimes.</dd>\n\n  <dt>Markdown in HTML</dt>\n  <dd>Does *not* work **very** well. Use HTML <em>tags</em>.</dd>\n</dl>  <video width=\"320\" height=\"240\" controls>\n  <source src=\"assets/pd1.webm\" type=\"video/webm\">\n  Your browser does not support the video tag.\n  </video>"
 },
 
 {
     "location": "Poincare/poincare.html#",
-    "page": "-",
-    "title": "-",
+    "page": "Poincare",
+    "title": "Poincare",
     "category": "page",
+    "text": ""
+},
+
+{
+    "location": "Poincare/poincare.html#Poincare-1",
+    "page": "Poincare",
+    "title": "Poincare",
+    "category": "section",
     "text": "Poincare section of a chaotic neuronal network. The domination of OpenCL in this benchmark might be due to a better use of vector intrinsics in Transpiler.jl, but needs some more investigations."
 },
 
 {
     "location": "SmokeSimulation/smokesimulation.html#",
-    "page": "-",
-    "title": "-",
+    "page": "Smoke Simulation",
+    "title": "Smoke Simulation",
     "category": "page",
-    "text": "<video width=\"320\" height=\"240\" controls>   <source src=\"assets/test.webm\" type=\"video/webm\"> Your browser does not support the video tag. </video>"
+    "text": ""
+},
+
+{
+    "location": "SmokeSimulation/smokesimulation.html#Smoke-Simulation-1",
+    "page": "Smoke Simulation",
+    "title": "Smoke Simulation",
+    "category": "section",
+    "text": "<video width=\"320\" height=\"240\" controls>\n  <source src=\"assets/test.webm\" type=\"video/webm\">\n  Your browser does not support the video tag.\n</video>"
 },
 
 ]}

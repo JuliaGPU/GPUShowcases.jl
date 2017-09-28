c397c5917e9c86b418ad322d58d2d8b0af686f83
autodocs <autodocs>
2017-09-28 14:11:15 +0000

    build based on 6c4ba79

---
+++ latest/search_index.js
@@ -0,0 +1,51 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Home",
+    "title": "Home",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "Convolution/convolution.html#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "PDE/pde.html#",
+    "page": "PDE 1",
+    "title": "PDE 1",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "PDE/pde.html#PDE-1-1",
+    "page": "PDE 1",
+    "title": "PDE 1",
+    "category": "section",
+    "text": "Show case ported from: https://github.com/johnfgibson/julia-pde-benchmark/blob/master/1-Kuramoto-Sivashinksy-benchmark.ipynb<video width=\"320\" height=\"240\" controls>   <source src=\"assets/pd1.webm\" type=\"video/webm\"> Your browser does not support the video tag. </video>"
+},
+
+{
+    "location": "Poincare/poincare.html#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "Poincare section of a chaotic neuronal network. The domination of OpenCL in this benchmark might be due to a better use of vector intrinsics in Transpiler.jl, but needs some more investigations."
+},
+
+{
+    "location": "SmokeSimulation/smokesimulation.html#",
+    "page": "-",
+    "title": "-",
+    "category": "page",
+    "text": "<video width=\"320\" height=\"240\" controls>   <source src=\"assets/test.webm\" type=\"video/webm\"> Your browser does not support the video tag. </video>"
+},
+
+]}

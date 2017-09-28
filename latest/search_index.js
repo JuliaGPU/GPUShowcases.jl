8c68b4639d1c19a87aabde0787fc98476ccc99e8
autodocs <autodocs>
2017-09-28 14:32:32 +0000

    build based on 101f55f

--- latest/search_index.js
+++ latest/search_index.js
@@ -29,7 +29,7 @@ var documenterSearchIndex = {"docs": [
     "page": "PDE 1",
     "title": "PDE 1",
     "category": "section",
-    "text": "Show case ported from:Kuramoto-Sivashinksy-benchmark<video width=\"320\" height=\"240\" controls> <source src=\"assets/pd1.webm\" type=\"video/webm\"> Your browser does not support the video tag. </video>"
+    "text": "Show case ported from:Kuramoto-Sivashinksy-benchmark<dl>   <dt>Definition list</dt>   <dd>Is something people use sometimes.</dd><dt>Markdown in HTML</dt>   <dd>Does not work very well. Use HTML <em>tags</em>.</dd> </dl><dl> <video width=\"320\" height=\"240\" controls> <source src=\"assets/pd1.webm\" type=\"video/webm\"> Your browser does not support the video tag. </video> </dl>"
 },
 
 {
